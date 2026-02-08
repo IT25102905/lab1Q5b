26af8ef9524583b6e3f89654b7f4e450a9c8f8fe
IT25102905 <IT25102905@my.sliit.lk>
2026-02-08 11:05:33 +0530

    IT25102905-Lab1Q5a

--- Calculate.java
+++ Calculate.java
@@ -1,37 +1,15 @@
 public class Calculate {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int value = (3 * 4 + 5 * 7);
-        int result = value * value;
+    int value =((4 + 7)^2 + (8 + 3)^2);
+    int result = value * value;
 
-        System.out.println("Result : " + result);
-        System.out.println("Method Result : " + Result());
-    }
+    System.out.println("Result : " + result);
+    System.out.println("Method Result : " + Result());
+  }
 
-    public static int Result() {
-        int value = (3 * 4 + 5 * 7);
-        return value * value;
-    }
+  public static int Result() {
+    int value = (3 * 4 + 5 * 7);
+    return value * value;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
