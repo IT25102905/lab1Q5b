08707159b12834dfaa761416c34a853004ab0a8c
IT25102905 <IT25102905@my.sliit.lk>
2026-02-08 11:03:17 +0530

    IT25102905-Lab1Q5b

---
+++ Calculate.java
@@ -0,0 +1,37 @@
+public class Calculate {
+    public static void main(String[] args) {
+
+        int value = (3 * 4 + 5 * 7);
+        int result = value * value;
+
+        System.out.println("Result : " + result);
+        System.out.println("Method Result : " + Result());
+    }
+
+    public static int Result() {
+        int value = (3 * 4 + 5 * 7);
+        return value * value;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
